1defb538bce610247ad35c81c9a8a1b1c6888682
suji1211 <borra.sujani@gmail.com>
2023-05-09 18:47:48 -0500

    frontend ansible

---
+++ env-dev/main.tfvars
@@ -0,0 +1,5 @@
+parameters =[
+{ name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.sujianilsrisriyaan.online:8080/"},
+{ name = "dev.frontend.cart_url", value = "http://cart-dev.sujianilsrisriyaan.online:8080/"}
+
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-name = "test.test1"
-type = "SecureString"
-value = "Hello world"
+resource "aws_ssm_parameter" "parameters" {
+count = length(var.parameters)
+name = var.parameters[count.index].name
+type = "String"
+value = var.parameters[count.index].value
 key_id = "3629063b-e9b6-4988-a8d4-96cb04b1190c"
 }
