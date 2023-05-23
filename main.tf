601d148c17a9cce8636f5a0239ecb57c91a537b3
suji <borra.sujani@gmail.com>
2023-05-23 09:13:50 -0400

    secret

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_ssm_parameter" "parameters" {
   key_id = "3629063b-e9b6-4988-a8d4-96cb04b1190c"
 }
 
-## passwords are not at all meant to keep inside the code at all..since it is a lab we are doing it to save the time
-# resource "aws_ssm_parameter" "passwords" {
-#   count = length(var.passwords)
-#   name = var.passwords[count.index].name
-#   value = var.passwords[count.index].value
-#   type = "SecureString"
-#   key_id = "3629063b-e9b6-4988-a8d4-96cb04b1190c"
-# }
+# passwords are not at all meant to keep inside the code at all..since it is a lab we are doing it to save the time
+ resource "aws_ssm_parameter" "passwords" {
+   count = length(var.passwords)
+   name = var.passwords[count.index].name
+   value = var.passwords[count.index].value
+   type = "SecureString"
+   key_id = "3629063b-e9b6-4988-a8d4-96cb04b1190c"
+ }
